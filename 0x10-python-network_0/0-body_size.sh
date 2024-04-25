840b4cd748720f83a931826f205ae127541b7909
wondwoesn <revatoel@gmail.com>
2024-04-25 14:48:17 +0300

    capture body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# send a request to URL via the command curl and capture the content length
+
+
+# this's a test for the number of args passed to curl
+if [ $# -ne 1 ]; then
+    echo "usage: $0 <URL>"
+    exit 1
+fi
+
+# assign the URL to the first arg
+url=$1
+
+# capture the content length in bytes in silent mode and dump to the null dir
+size=$(curl -s -w "%{size_download}" -o /dev/null "$url")
+
+# display size
+echo "size of the response body: $size bytes"
