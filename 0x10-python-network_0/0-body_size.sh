c044da3187fe24d238855db99a51cb3057c281c0
Wondwosen <111625259+mc-solo@users.noreply.github.com>
2024-04-25 20:53:23 +0300

    fix content-length 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# send a GET request to the url specified and print the size of response body to stdout
-curl -sI "$1" | grep -i "Content-Length" | cut -d ":" -f 2
-
+# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
