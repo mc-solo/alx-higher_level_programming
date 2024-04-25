72127f8814358cf3084e07f5b6dfd17b4f38a411
wondwoesn <revatoel@gmail.com>
2024-04-25 14:59:10 +0300

    change the whole approach

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# send a request to URL via the command curl and capture the content length
-
-
-# this's a test for the number of args passed to curl
-if [ $# -ne 1 ]; then
-    echo "usage: $0 <URL>"
-    exit 1
-fi
-
-# assign the URL to the first arg
-url=$1
-
-# capture the content length in bytes in silent mode and dump to the null dir
-size=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-
-# display size
-echo $size
+# send a GET request to the url specified and print the size of response body to stdout
+curl -sI "$1" | grep -i "Content-Length" | cut -d ":" -f 2
