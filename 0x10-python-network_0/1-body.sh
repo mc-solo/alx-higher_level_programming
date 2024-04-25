87335292581b6f968bf7d815e2a2f108a89f2038
wondwoesn <revatoel@gmail.com>
2024-04-25 21:20:59 +0300

    check if status code is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                   
 # display the body of the response in silence mode
-curl -s -o /dev/stdout -w "%{http_code}" "$1" 
+curl -s -o /dev/stdout -w "%{http_code}" "$1" | awk '$NF == 200'
